735c44f0b2b22aac174bfde57adaf7cca653bf53
tomer91131 <81261164+tomer91131@users.noreply.github.com>
2021-08-30 18:07:17 +0300

    Day 1 end
    learned main concepts chapters 1-3

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -3,4 +3,33 @@ import ReactDOM from 'react-dom';
 
 const myfirstelement = <h1>Hello React!</h1>
 
-ReactDOM.render(myfirstelement, document.getElementById('root'));
+const name = "tomer abrashkin"
+const user = {
+  firstname: "tomer",
+  lastname: "abrashkin"
+};
+const element = (
+  <h1 className="greeting">
+    Hello, world!
+  </h1>
+);
+
+const presentme = (
+  <h1>Hello, Mr. {user.firstname +" " + user.lastname} !</h1>
+);
+
+function clock(){
+  const time_element = (
+    <div>
+      <h1>It is {new Date().getSeconds()}:{new Date().getMinutes()}:{new Date().getHours()} in israel</h1>
+    </div>
+  );
+  ReactDOM.render(time_element,document.getElementById('root'));
+}
+
+setInterval(clock,1000)
+
+// ReactDOM.render(myfirstelement,document.getElementById('root'));
+// ReactDOM.render(presentme, document.getElementById('root'));
+// ReactDOM.render(element, document.getElementById('root'));
+
